b04b943229f84ba3669786ca5c43029d218df7cf
Josh6 <42172453+josuembuyu@users.noreply.github.com>
2019-12-27 18:37:25 +0100

    Dynamisation menu

--- public/javascripts/api/users_api.js
+++ public/javascripts/api/users_api.js
@@ -162,14 +162,127 @@ const getUserInfos = (user_id, callback) => {
 const getNav = () => {
 
 	getUserId(function (state, user) {
-        console.log(state);
 		var navContent;
 		if (state) {
 			//Recuperation des informations du user
 			getUserInfos(user.user_id, function (infos) {
-                console.log("infos");
+                console.log(infos)
                 if (infos.getEtat) {
+                    navContent = `<!--  User Notifications -->
+                                <div class="header-widget hide-on-mobile">
+                                   
+                                    <!-- Messages -->
+                                    <div id="containerMessage" class="header-notifications">
+                                        <div class="header-notifications-trigger">
+                                            <a id="LinkMessage" href="#"><i class="icon-feather-mail"></i><span>3</span></a>
+                                        </div>
 
+                                        <!-- Dropdown -->
+                                        <div class="header-notifications-dropdown">
+
+                                            <div class="header-notifications-headline">
+                                                <h4>Messages</h4>
+                                                <button class="mark-as-read ripple-effect-dark" title="Mark all as read" data-tippy-placement="left">
+                                                    <i class="icon-feather-check-square"></i>
+                                                </button>
+                                            </div>
+
+                                            <div class="header-notifications-content">
+                                                <div class="header-notifications-scroll" data-simplebar>
+                                                    <ul>
+                                                        <!-- Notification -->
+                                                        <li class="notifications-not-read">
+                                                            <a href="dashboard-messages.html">
+                                                                <span class="notification-avatar status-online"><img src="images/user-avatar-small-03.jpg" alt=""></span>
+                                                                <div class="notification-text">
+                                                                    <strong>David Peterson</strong>
+                                                                    <p class="notification-msg-text">Thanks for reaching out. I'm quite busy right now on many...</p>
+                                                                    <span class="color">4 hours ago</span>
+                                                                </div>
+                                                            </a>
+                                                        </li>
+
+                                                        <!-- Notification -->
+                                                        <li class="notifications-not-read">
+                                                            <a href="dashboard-messages.html">
+                                                                <span class="notification-avatar status-offline"><img src="images/user-avatar-small-02.jpg" alt=""></span>
+                                                                <div class="notification-text">
+                                                                    <strong>Sindy Forest</strong>
+                                                                    <p class="notification-msg-text">Hi Tom! Hate to break it to you, but I'm actually on vacation until...</p>
+                                                                    <span class="color">Yesterday</span>
+                                                                </div>
+                                                            </a>
+                                                        </li>
+
+                                                        <!-- Notification -->
+                                                        <li class="notifications-not-read">
+                                                            <a href="dashboard-messages.html">
+                                                                <span class="notification-avatar status-online"><img src="images/user-avatar-placeholder.png" alt=""></span>
+                                                                <div class="notification-text">
+                                                                    <strong>Marcin Kowalski</strong>
+                                                                    <p class="notification-msg-text">I received payment. Thanks for cooperation!</p>
+                                                                    <span class="color">Yesterday</span>
+                                                                </div>
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+                                            <a href="dashboard-messages.html" class="header-notifications-button ripple-effect button-sliding-icon">View All Messages<i class="icon-material-outline-arrow-right-alt"></i></a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <!--  User Notifications / End -->
+
+                                <!-- User Menu -->
+                                <div class="header-widget">
+
+                                    <!-- Messages -->
+                                    <div id="ContentUserDropdown" class="header-notifications user-menu">
+                                        <div class="header-notifications-trigger">
+                                            <a id="linkUser" href="#"><div class="user-avatar status-online"><img src="images/user-avatar-small-01.jpg" alt=""></div></a>
+                                        </div>
+
+                                        <!-- Dropdown -->
+                                        <div class="header-notifications-dropdown">
+
+                                            <!-- User Status -->
+                                            <div class="user-status">
+
+                                                <!-- User Name / Avatar -->
+                                                <div class="user-details">
+                                                    <div class="user-avatar status-online"><img src="images/user-avatar-small-01.jpg" alt=""></div>
+                                                    <div class="user-name">
+                                                        ${infos.getObjet.email} <span>${infos.getObjet.typeUser}</span>
+                                                    </div>
+                                                </div>
+                                                
+                                                <!-- User Status Switcher -->
+                                                <div class="status-switch" id="snackbar-user-status">
+                                                    <label class="user-online current-status">Online</label>
+                                                    <label class="user-invisible">Invisible</label>
+                                                    <!-- Status Indicator -->
+                                                    <span class="status-indicator" aria-hidden="true"></span>
+                                                </div>  
+                                        </div>
+                                        
+                                        <ul class="user-menu-small-nav">
+                                            <li><a href="/profile/${user.user_id}/dashboard"><i class="icon-material-outline-dashboard"></i> Dashboard</a></li>
+                                            <li><a href="/profile/${user.user_id}/parametres"><i class="icon-material-outline-settings"></i> Settings</a></li>
+                                            <li><a href="/logout"><i class="icon-material-outline-power-settings-new"></i> Logout</a></li>
+                                        </ul>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <!-- User Menu / End -->`;
+                    $("#navMenu").prepend(navContent);
+
+                    toggleDropdown("containerMessage","LinkMessage");
+                    toggleDropdown("ContentUserDropdown", "linkUser");
                 }else{
                     navContent = `<!-- Lien vers l'activation d'un compte -->
                     <div class="header-widget hide-on-mobile">
@@ -184,11 +297,21 @@ const getNav = () => {
                 <div class="header-widget">
                     <a href="#sign-in-dialog" class="popup-with-zoom-anim log-in-button"><i class="icon-feather-log-in"></i> <span>Connexion / Inscription</span></a>
                 </div>`;
-			//$("#navMenu").prepend(navContent);
+			$("#navMenu").prepend(navContent);
 		}
 	});
 }
+/**
+ * Cette fonction permet de faire le toggle des dropdown
+ */
+ const toggleDropdown = (content, link) => {
+    $("#" + link).on('click', function (e) {
+        e.preventDefault();
 
+        $("#" + content)[0].classList.toggle('active');
+       
+    });
+ }
 /**
  * Module permettant d'activer un compte utilisateur
  */
