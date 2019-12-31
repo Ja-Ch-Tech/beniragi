dfc2a717f29e5c4d80c0a22422ba8b54c3dd1240
jemima mbuyu <15md244@esisalama.org>
2019-12-31 00:55:45 -0200

    Modification design

--- public/javascripts/api/init.js
+++ public/javascripts/api/init.js
@@ -1,7 +1,7 @@
 
 const getHostApi = () => {
-    //return "http://localhost:3456/";
-    return "https://api-beniragi-service.herokuapp.com/";
+    return "http://localhost:3456/";
+    //return "https://api-beniragi-service.herokuapp.com/";
 }
 
 const getHostWeb = () => {
